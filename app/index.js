e38e51a2982cfa5da3d5bc3e966f03c2ad479332
louloutozor <lsda0191@hotmail.fr>
2017-06-30 15:37:33 +0200

    fix

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ const ArgumentParser = require('argparse').ArgumentParser;
 const authenticationController = require('./controllers/authentificationCtrl');
 const indexController = require('./controllers/indexCtrl');
 
+
 //dunno what is dat
 const config = {
    defaultName: 'index/index'
@@ -28,18 +29,14 @@ app.get('/list', authenticationCtrl.postlogin.bind(authenticationCtrl));
 
 app.post('/registration', authenticationCtrl.postregistration.bind(authenticationCtrl));
 app.post('/login', authenticationCtrl.postlogin.bind(authenticationCtrl));
-app.post('/list', authenticationCtrl.postlogin.bind(authenticationCtrl));
+
 
 const indexCtrl = new indexController(config);
 app.get('/', indexCtrl.index.bind(indexCtrl));
 
 
 //recup le csv //todo comparer data avec données saisie
-const csvDB = new csv_db('input.csv', ['mail', 'password']);
-csvDB.get().then((data) => {
-   console.log(data)
-}, (err) => {
-   console.log(err)
-})
+
+
 
 app.listen(3000);
