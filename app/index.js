7ca42755ee9ceeac58c83b450d8c61321e3f3b04
Maxime <maxime.lopez44@gmail.com>
2017-06-30 11:42:57 +0200

    update app/index.js delete views/index.js

--- app/index.js
+++ app/index.js
@@ -9,15 +9,13 @@ const config = {
    defaultName: 'test'
 };
 
-
-
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/../views'));
 
 const authenticationCtrl = new authenticationController(config);
-app.get('/registration', authenticationCtrl.register.bind(authenticationCtrl));
+app.get('/registration', authenticationCtrl.registration.bind(authenticationCtrl));
 app.get('/login', authenticationCtrl.login.bind(authenticationCtrl));
 
 app.listen(3000);
