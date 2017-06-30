81ee07c4f90099daa5c4602f1cf259a95a1c5704
gaetank <gaetan.kemayou@hotmail.fr>
2017-06-30 12:21:28 +0200

    refacto

--- app/controllers/authentificationCtrl.js
+++ app/controllers/authentificationCtrl.js
@@ -1,6 +1,6 @@
 const Controller = require('./Controller');
 const csv_db = require('csv-db');
-const csvDb = new csv_db('emails.csv', ['id', 'username', 'password']);
+const csvDb = new csv_db('emails.csv', ['pseudo', 'email', 'password']);
 
 class AuthentificationCtrl extends Controller {
 
@@ -11,11 +11,18 @@ class AuthentificationCtrl extends Controller {
 
 	postRegistration(req, res) {
 		console.log("in the post registration");
+		
 		const user = {
 			pseudo: req.body.pseudo,
 			email: req.body.email,
 			password: req.body.password
 		};
+
+		csvDb.insert(user).then((data) => {
+		  console.log(data);
+		}, (err) => {
+		  console.log(err);
+		});
 	}
 
 	login(req, res){
