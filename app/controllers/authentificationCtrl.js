366c224277e7f13bf7a6c4691ec8a2f5706922af
louloutozor <lsda0191@hotmail.fr>
2017-07-17 14:59:04 +0200

    psotlogin

--- app/controllers/authentificationCtrl.js
+++ app/controllers/authentificationCtrl.js
@@ -2,7 +2,6 @@ const Controller = require('./Controller');
 const csv_db = require('csv-db');
 const fs = require('fs');
 const path = require('path');
-
 const pathToFile = path.join(__dirname, 'emails.csv');
 const csvDb = new csv_db(pathToFile, ['id', 'pseudo', 'password']);
 
@@ -17,12 +16,12 @@ class AuthentificationCtrl extends Controller {
 	}
 	
     login(req, res){
+        console.log('login');
         res.render('authentification/login');
     }
 
-
 	postregistration(req, res) {		
-
+		console.log("postregistration");
 		if(!fs.existsSync(pathToFile)) {
     		fs.openSync(pathToFile, 'w');
 		}
@@ -41,34 +40,13 @@ class AuthentificationCtrl extends Controller {
 
 	}
 
-
-
 	postlogin(req, res){
 
-		const USER = {
-            id: req.body.email,
-            pseudo: req.body.pseudo,
-            password: req.body.password
-        };
-
-        //if (csvDb.get(req.body.email) === USER) { console.log("god damn it"); }
-
-
-        console.log("field " + req.body.email)
-        console.log("success "+csvDb.get(req.body.email));
+        console.log("field " + req.body.email);
 		//if(csv.get(req.body.email).email === req.body.email) { console.log("god damn it");}
-		//console.log("success "+req.body.email);
 
 		res.render('emailList/emailList')
 	}
 
-    emailList(req, res) {
-        res.render('emailList/emailList');
-    }
-
-    postemailList(req, res) {
-
-    }
-	
 }
 module.exports = AuthentificationCtrl;
